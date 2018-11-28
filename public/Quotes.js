1b84e74036bdbafebc3bf162784fba8fa5b767ae
RizkyAnanda1432 <rizkyanandaa002@gmail.com>
2018-11-28 14:05:21 +0700

    Quotes Rizky Ananda

---
+++ public/Quotes.js
@@ -0,0 +1,26 @@
+function gombal()
+    {
+        var quotes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        var index =  Math.floor(Math.random() * quotes.length)
+        var hasil = quotes[index]
+        document.getElementById('img').setAttribute("src",hasil+".png")
+        return hasil
+    } 
+
+function lucu()
+    {
+        var quotes = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+        var index =  Math.floor(Math.random() * quotes.length)
+        var hasil = quotes[index]
+        document.getElementById('img').setAttribute("src",hasil+".png")
+        return hasil
+    } 
+
+function bijak()
+    {
+        var quotes = ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
+        var index =  Math.floor(Math.random() * quotes.length)
+        var hasil = quotes[index]
+        document.getElementById('img').setAttribute("src",hasil+".png")
+        return hasil
+    } 
